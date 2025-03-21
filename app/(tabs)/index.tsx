1b8ef34dcecd42c8c06efa58ae860198a28d5d10
Faridbhp <farid.bhp9431@gmail.com>
2025-03-21 15:04:41 +0700

    Show Trending Movies
    - create function to get data trending movies in appwrite
    - cretae trendingCard
    - penyesuaian tampilan home dengan ada trending

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,6 +14,8 @@ import SearchBar from "../components/SearchBar";
 import useFetch from "@/services/useFetch";
 import { fetchPopularMovies } from "@/services/api";
 import MovieCard from "../components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
+import TrendingCard from "../components/TrendingCard";
 
 export default function Index() {
     const router = useRouter();
@@ -27,6 +29,12 @@ export default function Index() {
             query: "",
         })
     );
+
+    const {
+        data: trendingMovies,
+        loading: trendingLoading,
+        error: trendingError,
+    } = useFetch(getTrendingMovies);
 
     return (
         <View className="flex-1 bg-primary">
@@ -43,15 +51,15 @@ export default function Index() {
                     className="w-12 h-10 mt-20 mb-5 mx-auto"
                 />
 
-                {moviesLoading ? (
+                {moviesLoading || trendingLoading ? (
                     <ActivityIndicator
                         size="large"
                         color="#0000ff"
                         className="mt-10 self-center"
                     />
-                ) : moviesError ? (
+                ) : moviesError || trendingError ? (
                     <Text className="text-lg text-red-500 text-center">
-                        Error: {moviesError?.message}
+                        Error: {moviesError?.message || trendingError?.message}
                     </Text>
                 ) : (
                     <View className="flex-1 mt-5">
@@ -60,6 +68,35 @@ export default function Index() {
                             placeholder="Search"
                         />
 
+                        {trendingMovies && (
+                            <View className="mt-10">
+                                <Text className="text-lg text-white font-bold mb-3">
+                                    Trending Movies
+                                </Text>
+
+                                <FlatList
+                                    horizontal
+                                    showsHorizontalScrollIndicator={false}
+                                    ItemSeparatorComponent={() => (
+                                        <View className="w-5" />
+                                    )}
+                                    className="mb4 mt-3"
+                                    data={trendingMovies}
+                                    renderItem={({ item, index }) => (
+                                        <TrendingCard
+                                            movie={item}
+                                            index={index}
+                                        />
+                                    )}
+                                    keyExtractor={(item) =>
+                                        `${item.movie_id.toString()}-${
+                                            item.searchTerm
+                                        }`
+                                    }
+                                />
+                            </View>
+                        )}
+
                         <>
                             <Text className="text-lg text-white font-bold mt-5 mb-3">
                                 Latest Movies
@@ -67,7 +104,9 @@ export default function Index() {
 
                             <FlatList
                                 data={movies}
-                                renderItem={({ item }) => <MovieCard {...item}/>}
+                                renderItem={({ item }) => (
+                                    <MovieCard {...item} />
+                                )}
                                 keyExtractor={(item) => item.id.toString()}
                                 numColumns={3}
                                 columnWrapperStyle={{
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -53,3 +53,20 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
     }
     1;
 };
+
+export const getTrendingMovies = async (): Promise<
+    TrendingMovie[] | undefined
+> => {
+    try {
+        const result = await database.listDocuments(
+            DATABASE_ID,
+            COLLECTION_ID,
+            [Query.limit(5), Query.orderDesc("count")]
+        );
+
+        return result.documents as unknown as TrendingMovie[];
+    } catch (error) {
+        console.log("error", error);
+        return undefined;
+    }
+};
