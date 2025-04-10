e74078ef6f5f989814b0ab783ec279105a36d3d8
Faridbhp <farid.bhp9431@gmail.com>
2025-04-10 11:16:39 +0700

    add SafeAreaView
    - untuk mengatasi tampilan terhalang oleh UI sistem operasi seperti status bar dll

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,18 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
     return (
-        <>
+        <SafeAreaView className="bg-primary flex-1">
             <StatusBar hidden={true} />
-            <Stack >
+            <Stack>
                 <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
                 <Stack.Screen
                     name="movies/[id]"
                     options={{ headerShown: false }}
                 />
             </Stack>
-        </>
+        </SafeAreaView>
     );
 }
