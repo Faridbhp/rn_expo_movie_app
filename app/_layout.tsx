e15b8afadca8a165470e5eff0085d21ae51473c7
Faridbhp <farid.bhp9431@gmail.com>
2025-04-14 08:35:29 +0700

    add SafeAreaProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,23 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
     return (
-        <SafeAreaView className="bg-primary flex-1">
-            <StatusBar hidden={true} />
-            <Stack>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                <Stack.Screen
-                    name="movies/[id]"
-                    options={{ headerShown: false }}
-                />
-            </Stack>
-        </SafeAreaView>
+        <SafeAreaProvider>
+            <SafeAreaView className="bg-primary flex-1">
+                <StatusBar hidden={true} />
+                <Stack>
+                    <Stack.Screen
+                        name="(tabs)"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="movies/[id]"
+                        options={{ headerShown: false }}
+                    />
+                </Stack>
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 }
