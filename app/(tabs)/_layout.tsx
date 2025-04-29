1f95646d49c85c7ba4bef01d171bc1c3f8d2d72e
Faridbhp <farid.bhp9431@gmail.com>
2025-04-29 09:44:46 +0700

    fix: update styling layout tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,55 +1,112 @@
-import { View, Text, ImageBackground, Image } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
-import { images } from '@/constants/images'
-import { icons } from '@/constants/icons'
+import { View, Text, ImageBackground, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 const TabIcon = ({ focused, icon, title }: any) => {
-    if (focused) {
-        return (
-            <ImageBackground source={images.highlight} className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden' >
                 <Image source={icon} tintColor="#151312" className='size-5' />
-                <Text className='text-secondary text-base font-semibold ml-2'>{title}</Text>
-            </ImageBackground>
-        )
-    }
-
     return (
-        <View className='size-full justify-center items-center mt-4 rounded-full'>
-            <Image source={icon} tintColor="#A8B5DB" className='size-5' />
+        <View className="flex flex-col items-center justify-cente">
+            <View
+                className={`rounded-full ${
+                    focused ? "bg-purple-600" : "bg-transparent"
+                } p-2 items-center justify-center`}>
+                <Image
+                    source={icon}
+                    tintColor={focused ? "#fff" : "#A8B5DB"}
+                    className={`${focused ? "size-8" : "size-5"}`}
+                />
+            </View>
+            {focused && (
+                <Text 
+                    numberOfLines={1} 
+                    className="text-white text-base font-semibold mt-2 w-16 text-center">
+                    {title}
+                </Text>
+            )}
         </View>
-    )
-}
+    );
+};
 
 const _Layout = () => {
     return (
-        <Tabs screenOptions={{
-            tabBarShowLabel: false,
-            tabBarItemStyle: { width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' },
-            tabBarStyle: { backgroundColor: '#0f0D23', borderRadius: 50, marginHorizontal: 20, marginBottom: 10, height: 52, position: 'absolute', overflow: 'hidden', borderWidth: 1, borderColor: '#0f0D23'}
-        }}>
-            <Tabs.Screen name='index' options={{
-                title: 'Home', headerShown: false, tabBarIcon: ({ focused }) => (
-                    <TabIcon focused={focused} icon={icons.home} title="Home" />
-                )
-            }} />
-            <Tabs.Screen name='search' options={{
-                title: 'Search', headerShown: false, tabBarIcon: ({ focused }) => (
-                    <TabIcon focused={focused} icon={icons.search} title="Search" />
-                )
-            }} />
-            <Tabs.Screen name='saved' options={{
-                title: 'Saved', headerShown: false, tabBarIcon: ({ focused }) => (
-                    <TabIcon focused={focused} icon={icons.save} title="Saved" />
-                )
-            }} />
-            <Tabs.Screen name='profile' options={{
-                title: 'Profile', headerShown: false, tabBarIcon: ({ focused }) => (
-                    <TabIcon focused={focused} icon={icons.person} title="Profile" />
-                )
-            }} />
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarItemStyle: {
+                    justifyContent: "center",
+                    alignItems: "center",
+                },
+                tabBarStyle: {
+                    backgroundColor: "#0f0D23",
+                    borderTopWidth: 0,
+                    elevation: 0,
+                    height: 60,
+                    position: "absolute",
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                },
+            }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "News",
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.home}
+                            title="News"
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: "Search",
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.search}
+                            title="Search"
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="saved"
+                options={{
+                    title: "Saved",
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.save}
+                            title="Saved"
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: "Profile",
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.person}
+                            title="Profile"
+                        />
+                    ),
+                }}
+            />
         </Tabs>
-    )
-}
+    );
+};
 
-export default _Layout
+export default _Layout;
