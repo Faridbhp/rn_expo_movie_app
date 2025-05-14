2c295640a08e5fa44686c9399dfe2f96aa5b5a01
Faridbhp <farid.bhp9431@gmail.com>
2025-05-14 11:07:00 +0700

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,9 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname)
 
+// Metro configuration for firebase expo
+// belum diperlukan, karena kalaupun tidak ada ini, tetap bisa jalan
+// config.resolver.sourceExts.push('cjs');
+// config.resolver.unstable_enablePackageExports = false;
+
 module.exports = withNativeWind(config, { input: './app/global.css' })
